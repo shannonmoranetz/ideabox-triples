126d04419e31eb43bf643abc4c6fb1558407f6d6
Whitney Burton <whitneyburton9@gmail.com>
2018-10-23 14:19:50 -0600

    Add card comp

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// var doc query title input, body input
+// event listener for save button 
+// createElement for new idea in response to save button
+
+var titleInput = document.getElementById(#title-input);
+var bodyInput = document.getElementById(#body-input);
+var saveButton = document.querySelector(.save-button);
+
+saveButton.addEventListener('click', function() {
+// save title function 
+// save body function 
+})
+
+function saveTitleInput() {
+  var 
+};
+
+function saveBodyInput() {
+
+};
