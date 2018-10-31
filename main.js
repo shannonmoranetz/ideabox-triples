5f829ea87594110c99f97181546b593d74a320b4
tim miller <tmiller12@gmail.com>
2018-10-31 15:25:09 -0600

    css and responsiveness

--- main.js
+++ main.js
@@ -103,15 +103,25 @@ function searchFilter() {
   });
 }
 
-function qualityFilter() {
+document.querySelector('.swill').addEventListener('click', function(e) {
   var qualityCategory = document.querySelectorAll('.quality-category');
   var swillButtonText = document.querySelector('.swill');
-  var plausibleButtonText = document.querySelector('.plausible');
-  var geniusButtonText = document.querySelector('.genius');
-}
+  e.preventDefault();
+  console.log(qualityCategory)
+    qualityCategory.forEach(function(qualityValue) {
+      if (document.querySelectorAll('.quality-category').innerText == swillButtonText.innerText) {
+        qualityCategory.parentNode.parentNode.parentNode.classList.add('display-mode-none')
+      }
+    })
+})
+
+  // var plausibleButtonText = document.querySelector('.plausible');
+  // var geniusButtonText = document.querySelector('.genius');
+  
 
 document.querySelector('.show-more-button').addEventListener('click', function(e) {
-  if (e.target.className === 'show-more-button') {
+  if (e.target.className === 'show-more-button buttons') {
+  console.log('fire')
     e.target.classList.add('hide-button');
     var getArticle = document.getElementById('card-article');
     getArticle.classList.remove('overflow');
